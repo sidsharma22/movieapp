533976255c5330b3e80426f18b4c00512c53101b
Sidharth Sharma <sidharthsharma@Sidharths-MacBook-Pro.local>
2022-01-30 18:28:19 +0700

    Header Section complete

--- components/Header.js
+++ components/Header.js
@@ -4,8 +4,8 @@ import HeaderIcon from './HeaderIcon';
 import { HomeIcon, UserIcon, PhoneIcon, InformationCircleIcon } from '@heroicons/react/solid'
 
 export default function Header() {
-  return <div>
-      <div className = "">
+  return <div className="bg-gray-700 text-gray-200 flex flex-col items-center p-6 select-none sm:flex-row justify-between">
+      <div className = "flex ">
         <HeaderIcon Icon= {HomeIcon} title="Home"/>
         <HeaderIcon Icon= {UserIcon} title="Account"/>
         <HeaderIcon Icon= {PhoneIcon} title="Contact"/>
@@ -16,6 +16,7 @@ export default function Header() {
       src = "https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
       width = {100}
       height = {100}
+      className="cursor-pointer active:brightness-200"
       />
   </div>;
 }
--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 export default function HeaderIcon({Icon, title}) {
-  return <div>
+  return <div className= "mx-4 felx flex-col cursor-pointer hover:text-teal-400 active:text-purple-600 lg:mx-6">
       <Icon className="h-8"/>
-      <p>{title}</p>
+      <p className="my-2">{title}</p>
 
   </div>;
 }
